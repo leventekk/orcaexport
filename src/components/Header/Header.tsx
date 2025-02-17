2a4a7898b815ea5ada719f5619b1c4017bd4adaf
levente kiraly <leventekk@users.noreply.github.com>
2025-02-17 14:53:26 +0100

    feat: update header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./header.module.css";
 export function Header() {
   return (
     <header className={styles.root}>
-      <h1>OrcaExport</h1>
+      <h1 className={styles.title}>OrcaExport</h1>
 
       <Actions />
     </header>
