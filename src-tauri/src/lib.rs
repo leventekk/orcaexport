abea0254cd39cbd6f732930643373a08f99b4d2a
levente kiraly <leventekk@users.noreply.github.com>
2025-02-17 10:43:13 +0100

    feat: enable devtools in debug mode

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,8 +4,14 @@ mod compress;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_prevent_default::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|_app| {
+            #[cfg(not(debug_assertions))]
+            {
+                _app.handle().plugin(tauri_plugin_prevent_default::init())?;
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![compress::export_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
